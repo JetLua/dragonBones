a13ca2089f8086642ddbc505a06f1679feb7887e
JetLu <i@lufei.im>
2021-04-17 01:21:40 +0800

    🔨 export namespace

--- dragonBones.d.ts
+++ dragonBones.d.ts
@@ -20,6 +20,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 declare namespace dragonBones {
     /**
      * @private
@@ -6555,4 +6556,4 @@ declare namespace dragonBones {
     }
 }
 
-export = dragonBones;
+export as namespace dragonBones;
